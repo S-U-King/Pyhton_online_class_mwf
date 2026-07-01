f83a400ab93137629cfd5c3112625735bb085e92
S.Umar Farooq <eabumar54@gmail.com>
2026-07-01 18:08:49 +0500

    python list assignment

---
+++ assignment9.py
@@ -0,0 +1,21 @@
+print("Assignment No : 8")
+
+print("Task 1: connect 2 lists")
+
+list1 = ["asad", "samad"]
+list2 = ["ahad", "sameer"]
+result = list1 + list2
+print(result)
+
+print("Task 2: remove 2nd index & add 2 values")
+
+studlist = ["asad", "samad", "ahad", "sameer"]
+studlist[2:3] = ["ali", "zain"]
+print(studlist)
+
+print("Task 3: remove 1st and last value")
+
+classroom = ["asad", "samad", "ahad", "sameer"]
+classroom.pop(0)
+classroom.pop()
+print(classroom)
